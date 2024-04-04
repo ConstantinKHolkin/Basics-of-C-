fa86743309727c2323869cbd0c9535e4a96c54aa
Konstantin Kholkin <konstantinkholkin@Konstantins-MacBook-Air.local>
2024-04-04 19:18:49 +0300

    add some variables lesson1:Task1

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Declaring variables and outputting them
+int a = 100;
+
+Console.WriteLine(a);
+
+double b = 5.5;
+char c = '!';
+bool d = true;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
