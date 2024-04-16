23fc27cf00c980943cb102c980439edd0ff96a0f
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-16 21:54:31 +0300

    Add Lesson3/Task3

---
+++ Lessons/Lesson3/Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = 0;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.Write($"{e} ");
+    }
+}
+int[] arry = {1, 2, 3, 4, 5};
+ZeroEvenElements(arry);
+PrintArray(arry);
