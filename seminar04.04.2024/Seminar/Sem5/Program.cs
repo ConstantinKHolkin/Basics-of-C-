c6ba5f8579adba98cffb127af099c0aa35a6e2ee
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-18 21:19:49 +0300

    Task 3 has been complete

--- seminar04.04.2024/Seminar/Sem5/Program.cs
+++ seminar04.04.2024/Seminar/Sem5/Program.cs
@@ -7,9 +7,20 @@
 //находящихся на главной диагонали (с индексами (0,0); (1;1) и
 //т.д.
 //Пример
-//2 3 4 3
- //4 3 4 1 => 2 + 3 + 5 = 10
- //2 9 5 4
+// 2 3 4 3
+// 4 3 4 1 => 2 + 3 + 5 = 10
+// 2 9 5 4
+
+// задача 3:
+// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// значений по строкам двумерного массива.
+// Пример
+// 2 3 4 3
+// 4 3 4 1
+// 2 3 4 3
+// 4 3 4 1 => [3 3 5]
+// 2 9 5 4
 
 int[,] CreateMatrix(int rows, int colons, int min, int max)
 {
@@ -22,7 +33,6 @@ int[,] CreateMatrix(int rows, int colons, int min, int max)
         }
     }
     return matrix;
-
 }
 
 void PrintMatrix(int[,] matrix)
@@ -33,8 +43,7 @@ void PrintMatrix(int[,] matrix)
         {
             System.Console.Write($"{matrix[i, j]} "); 
         }
-        System.Console.WriteLine();
-        
+        System.Console.WriteLine(); 
     }
 }
 
@@ -48,11 +57,9 @@ int[,] EvenIndxSquare(int[,] matrix)
             {
                 matrix[i,j] = matrix[i,j] * matrix[i,j];
             }
-        }
-        
+        }    
     }
     return matrix;
-
 }
 
 int SumMatrix(int[,] matrix)
@@ -64,13 +71,34 @@ int SumMatrix(int[,] matrix)
         {
             if (i == j)
             {
-               sum = sum +matrix[i,j];
+               sum += matrix[i,j];
             }
-        }
-        
+        }   
     }
     return sum;
+}
 
+double[] MidlSumArray (int[,] matrix)
+{
+    double[] arr = new double[matrix.GetLength(0)];
+    double summatrix = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summatrix += matrix[i,j];
+        }   
+        arr [i] = summatrix/matrix.GetLength(1);
+        summatrix = 0;
+    }
+    return arr;
+}
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
 }
 
 System.Console.WriteLine("Input matrix rows: ");
@@ -86,4 +114,4 @@ int [,] matrix = CreateMatrix(rows,colons, min, max);
 PrintMatrix (matrix);
 PrintMatrix(EvenIndxSquare(matrix));
 System.Console.WriteLine(SumMatrix(matrix));
-
+PrintArray(MidlSumArray(matrix));
