4e0f4d8808b40c14b9894054069399f08052b0da
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-20 21:20:35 +0300

    Add Lesson4/Task1

---
+++ Lessons/Lesson4/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount,columsCount];  
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+int [,] matrix = CreateMatrix(3,5);
+PrintMatrix(matrix);
