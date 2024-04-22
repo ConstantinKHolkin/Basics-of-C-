832ddb2b8cef0ddb19461c0f03e1830c9a8ccffc
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-22 20:56:06 +0300

    Fix Sem6

--- seminar04.04.2024/Seminar/Sem6/Program.cs
+++ seminar04.04.2024/Seminar/Sem6/Program.cs
@@ -5,29 +5,29 @@
 // Пример
 // [‘a’, ‘b’, ‘c’, ‘d’] => “abcd”
 
-// string CharArrayToString(char[] charArray)
-// {
-//     string result = string.Empty;
-//     for (int i = 0; i < charArray.Length; i++)
-//     {
-//         result = result + charArray[i];
-//     }
-//     return result;
-// }
+string CharArrayToString(char[] charArray)
+{
+    string result = string.Empty;
+    for (int i = 0; i < charArray.Length; i++)
+    {
+        result = result + charArray[i];
+    }
+    return result;
+}
 
-// char[] InputCharArray()
-// {
-//     System.Console.WriteLine("Input number of array size: ");
-//     int size = Convert.ToInt32(Console.ReadLine());
-//     char[] array = new char[size];
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         array[i] = Convert.ToChar(Console.ReadLine());
-//     }
-//     return array;
-// }
+char[] InputCharArray()
+{
+    System.Console.WriteLine("Input number of array size: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    char[] array = new char[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToChar(Console.ReadLine());
+    }
+    return array;
+}
 
-// System.Console.WriteLine(CharArrayToString(InputCharArray()));
+System.Console.WriteLine(CharArrayToString(InputCharArray()));
 
 
 // На основе символов строки (тип string) сформировать массив
@@ -37,26 +37,26 @@
 // Пример
 // “Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ]
 
-// char[] StringToCharArray (string str)
-// {
-//     char[] chars = new char[str.Length];
-//     for (int i = 0; i < str.Length; i++)
-//     {
-//         chars[i] = str[i];
-//     }
-//     return chars;
-// }
+char[] StringToCharArray (string str)
+{
+    char[] chars = new char[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        chars[i] = str[i];
+    }
+    return chars;
+}
 
-// void PrintCharsArray(char[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         System.Console.Write(" ' " + array[i] + " ' ");
-//     }
-//     System.Console.WriteLine();
-// }
+void PrintCharsArray(char[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(" ' " + array[i] + " ' ");
+    }
+    System.Console.WriteLine();
+}
 
-// PrintCharsArray(StringToCharArray("Hello!"));
+PrintCharsArray(StringToCharArray("Hello!"));
 
 // Считать строку с консоли, состоящую из латинских
 // букв в нижнем регистре. Выяснить, сколько среди
