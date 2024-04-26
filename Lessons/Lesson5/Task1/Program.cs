eb3dabd10cb775eed3a30ec52ffca2316bb0e9f6
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-26 21:51:41 +0300

    Task 1+comments Lessons/Lesson5

--- Lessons/Lesson5/Task1/Program.cs
+++ Lessons/Lesson5/Task1/Program.cs
@@ -1,10 +1,10 @@
-﻿int FactNumber (int n)
+﻿int FactNumber (int n) //Метод факториала числа (Рекурсия)
 {
-    if (n == 1 || n == 0)   
+    if (n == 1 || n == 0) // Условия остановки
     {
         return 1;
     }
-     return n * FactNumber(n-1);
+     return n * FactNumber(n-1); //Рекурсия
      
     
 }
