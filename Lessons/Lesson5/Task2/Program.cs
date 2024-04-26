7d7e661ca429e18b257e9ffb17f8a6166be721c5
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-26 23:02:15 +0300

    Add Lesson5/Task2

---
+++ Lessons/Lesson5/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿void OpenMatryoshca (int size)
+{
+    if (size == 0)
+    {
+        return;
+    }
+
+    System.Console.WriteLine($"Open matryoshca of {size}");
+    OpenMatryoshca(size - 1);
+}
+
+OpenMatryoshca(5);
