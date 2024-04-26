b97b3f6acffc4044c9d13108e5f09e9fcd9e2766
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-26 21:43:47 +0300

    Task 1 Lessons/Lesson5

---
+++ Lessons/Lesson5/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int FactNumber (int n)
+{
+    if (n == 1 || n == 0)   
+    {
+        return 1;
+    }
+     return n * FactNumber(n-1);
+     
+    
+}
+
+System.Console.WriteLine(FactNumber(5));
