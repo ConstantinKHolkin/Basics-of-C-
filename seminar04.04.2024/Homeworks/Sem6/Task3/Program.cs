40c8832e3c105d92a032bd19afab7cd7cd980e99
Konstantin KHolkin <constantineholkin@yandex.com>
2024-04-30 13:26:51 +0300

    Add Homeworks/Sem6/Task3

---
+++ seminar04.04.2024/Homeworks/Sem6/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+string ReverseString(string str)
+{
+    char[] charArray = str.ToCharArray();
+    Array.Reverse(charArray);
+    return new string(charArray);
+}
+string SimplifyingTheString(string str)
+{
+    str = str.Replace(" ", "").ToLower();
+    return str;
+}
+void Comparison (string str, string str2)
+{
+    if (str == str2)
+    {
+        System.Console.WriteLine("Строка является палиндромом.");
+    }
+    else 
+    {
+        System.Console.WriteLine("Строка не является палиндромом.");
+    }
+}
+
+// Console.InputEncoding = System.Text.Encoding.GetEncording("utf-16");
+System.Console.WriteLine("Введите строку: ");
+string str = Console.ReadLine();
+str = SimplifyingTheString(str);
+string str2 = ReverseString(str);
+Comparison(str, str2);
+   
+   
