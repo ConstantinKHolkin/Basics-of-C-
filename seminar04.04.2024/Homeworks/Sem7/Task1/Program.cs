6e96c143f69a05614cea91635ef1e33e258d6042
Konstantin KHolkin <constantineholkin@yandex.com>
2024-05-04 13:34:08 +0300

    add Homeworks/Sem7/Task1

---
+++ seminar04.04.2024/Homeworks/Sem7/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void SearchForNaturalNumbers (int m,int n)
+{
+    if (m > 0 && n > 0)
+    {
+        if (n < m)
+        {
+            SearchForNaturalNumbers(m - 1, n);
+            System.Console.Write(m + " ");
+        }
+        else if (n > m)
+        {
+            SearchForNaturalNumbers(m, n - 1);
+            System.Console.Write(n + " ");
+        }
+        else
+        {
+            System.Console.Write(m + " ");
+        }
+    }
+    else 
+    {
+        System.Console.WriteLine("There are no natural numbers!");
+    }
+
+}
+
+
+System.Console.WriteLine("Input number M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Input number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SearchForNaturalNumbers (m, n);
